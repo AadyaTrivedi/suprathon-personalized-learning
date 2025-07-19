19e3594886d7856229a127d6a17594a9e806c03e
Aadya Trivedi <134907107+AadyaTrivedi@users.noreply.github.com>
2025-07-19 16:43:54 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-12 bg-white text-center">
-      <h1 className="text-4xl font-bold mb-4 text-black-700">🎓 NeuroLearn</h1>
+      <h1 className="text-4xl font-bold mb-4 text-indigo-700">🎓 NeuroLearn</h1>
       <p className="text-lg mb-6 text-gray-700">
-        Discover your personalised learning style and study smarter.
+        Discover your personalized learning style and study smarter.
       </p>
       <a
         href="/results"
-        className="bg-black-600 text-white px-6 py-3 rounded-full hover:bg-gray-700 transition"
+        className="bg-indigo-600 text-white px-6 py-3 rounded-full hover:bg-indigo-700 transition"
       >
         Start Quiz →
       </a>
